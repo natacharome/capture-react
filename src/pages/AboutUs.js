3493314996c3ff552983971f3296b3b6a918f2f6
Natacha Rome <natacha-rome@hotmail.fr>
2020-12-09 15:53:26 +0100

    Faq section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,14 +1,15 @@
 import React from "react";
 import About from "../components/About";
-import Service from "../components/Service"
+import Service from "../components/Service";
+import Faq from "../components/Faq";
 
 const AboutUs = () => {
     return (
         <>
             <About/> 
             <Service /> 
+            <Faq /> 
         </>
-      
     )
 }
 
