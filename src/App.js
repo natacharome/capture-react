217a8c33420d3cd510fd1b4743dbc57a16e75132
Natacha Rome <natacha-rome@hotmail.fr>
2020-12-10 15:13:11 +0100

    Navigation with react router DOM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 import AboutUs from  "./pages/AboutUs";
+import ContactUs from  "./pages/ContactUs";
+import OurWork from  "./pages/OurWork";
+import Nav from  "./components/Nav";
+
+// Routing 
+import { Switch, Route } from 'react-router-dom';
 
 //Import global style
 import GlobalStyle from  "./components/GlobalStyle";
@@ -7,8 +13,19 @@ import GlobalStyle from  "./components/GlobalStyle";
 function App() {
   return (
     <div className="App">
-      <GlobalStyle/>
-      <AboutUs/>
+       <GlobalStyle/>
+       <Nav/>
+        <Switch>
+          <Route path="/" exact>
+            <AboutUs/>  
+          </Route>
+          <Route path="/work">
+            <OurWork/>
+          </Route>
+          <Route path="/contact">
+            <ContactUs/>
+          </Route>
+        </Switch>
     </div>
   );
 }
