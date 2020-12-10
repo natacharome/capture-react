686ebce35fcf95f4de56b6fac5533548786ca777
Natacha Rome <natacha-rome@hotmail.fr>
2020-12-10 11:41:00 +0100

    Style Faq section

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+// Styles
+import styled from "styled-components";
+import {AboutSection} from "../styles";
 const Faq = () => {
     return (
-        <div className="faq">
+        <FaqSection>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How do I start ?</h4>
@@ -10,6 +13,7 @@ const Faq = () => {
                     <p>Lorem ipsum, dolor sit amet.</p>
                     <p>Inter haec Orfitus praefecti potestate regebat urbem aeternam ultra modum delatae dignitatis sese efferens insolenter, vir quidem prudens et forensium negotiorum oppido gnarus.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily Schedule</h4>
@@ -17,6 +21,7 @@ const Faq = () => {
                     <p>Lorem ipsum, dolor sit amet.</p>
                     <p>Inter haec Orfitus praefecti potestate regebat urbem aeternam ultra modum delatae dignitatis sese efferens insolenter, vir quidem prudens et forensium negotiorum oppido gnarus.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Different payement methods</h4>
@@ -24,6 +29,7 @@ const Faq = () => {
                     <p>Lorem ipsum, dolor sit amet.</p>
                     <p>Inter haec Orfitus praefecti potestate regebat urbem aeternam ultra modum delatae dignitatis sese efferens insolenter, vir quidem prudens et forensium negotiorum oppido gnarus.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What products do you offer ?</h4>
@@ -31,9 +37,38 @@ const Faq = () => {
                     <p>Lorem ipsum, dolor sit amet.</p>
                     <p>Inter haec Orfitus praefecti potestate regebat urbem aeternam ultra modum delatae dignitatis sese efferens insolenter, vir quidem prudens et forensium negotiorum oppido gnarus.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </FaqSection>
     )
 }
+
+const FaqSection = styled(AboutSection)`
+    display: block;
+    span {
+        display: block;
+    }
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line {
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question {
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer {
+        padding: 2rem 0rem;
+        p {
+            padding: 1rem 0rem;
+        }
+    }
+
+`
 
 export default Faq;
